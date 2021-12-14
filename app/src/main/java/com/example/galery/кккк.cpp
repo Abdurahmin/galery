f073166421744369dea134a2e13ff484e7570b77
nurdinghejjr <rimbekovilzar@gmail.com>
2021-12-14 20:35:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/galery/кккк.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Admin on 25.10.2021.
+//
+
+#include "кккк.h"
---
+++ app/src/main/java/com/example/galery/кккк.h
@@ -0,0 +1,16 @@
+//
+// Created by Admin on 25.10.2021.
+//
+
+#ifndef GALERY_КККК_H
+#define GALERY_КККК_H
+
+
+
+class кккк {
+
+};
+
+
+
+#endif //GALERY_КККК_H
